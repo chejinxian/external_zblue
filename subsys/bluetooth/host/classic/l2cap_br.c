8e282b39199fccebc32fffdb0603a5968e6a6598
Johan Hedberg <johan.hedberg@silabs.com>
2025-03-18 10:53:46 +0200

    Bluetooth: Host: Fix format specifier warnings
    Use %zu for size_t and %zd for ssize_t, and don't use those specifiers for
    any other type. Also make sure that field width specifiers get typecast to
    the expected int type.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@silabs.com>

--- subsys/bluetooth/host/classic/l2cap_br.c
+++ subsys/bluetooth/host/classic/l2cap_br.c
@@ -329,7 +329,7 @@ int bt_l2cap_br_send_cb(struct bt_conn *conn, uint16_t cid, struct net_buf *buf,
 		return -ESHUTDOWN;
 	}
 
-	LOG_DBG("chan %p buf %p len %zu", br_chan, buf, buf->len);
+	LOG_DBG("chan %p buf %p len %u", br_chan, buf, buf->len);
 
 	hdr = net_buf_push(buf, sizeof(*hdr));
 	hdr->len = sys_cpu_to_le16(buf->len - sizeof(*hdr));
